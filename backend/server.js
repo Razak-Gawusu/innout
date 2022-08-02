a74c48363a45e9c5aaaf739f19b22f3483af22f5
Razak-Gawusu <gawusur@gmail.com>
2022-08-02 21:42:21 +0100

    preparing to deploy

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/allProducts', allProductsRouter)
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production'))
 }
