90abfe2e48191b724547edd18786221eae354faf
Razak-Gawusu <gawusularbil@gmail.com>
2022-09-02 16:21:31 +0100

    deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true})
-        console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline)
+        console.log(`MongoDB connected`)
     } catch (error) {
         console.log(error)
         process.exit(1)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
 const dotenv = require('dotenv').config()
 const app = express()
 const fileUpload = require('express-fileupload')
