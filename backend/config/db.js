ff7a34627769c577face27cc4ec3cbd74ef06607
Razak-Gawusu <gawusularbil@gmail.com>
2022-09-12 22:22:43 +0100

    built

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+        const conn = await mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true})
         console.log(`MongoDB connected`)
     } catch (error) {
         console.log(error)
